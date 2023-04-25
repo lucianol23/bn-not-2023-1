dc9f803c055b31e294751033594cfa4c10d2abf2
lucianol23 <102415218+lucianol23@users.noreply.github.com>
2023-04-25 02:03:55 +0000

    alteração

--- back-end/controllers/sale.js
+++ back-end/controllers/sale.js
@@ -60,7 +60,49 @@ controller.retriveOne = async (req, res) =>{
 controller.update = async (req, res) =>{
     try{
         
-        const result = await Sale.findByIdAndUpdate(req.params.id, req.body)
+        // Encontra a venda para ser atualizado
+        const sale = await Sale.findById(req.params.id)
+        // Se itens tiver sido passado no body
+        if(req.body.items){
+            //percorre cada item de req.body, verificando se ja existe
+            // ou não em sales.item
+            for(let item of req.body.items){
+                //Se o item tem _id é porque ja existe ~> É caso de atualização
+                if(item._id){
+                    
+                    //Verifica se foi passado uma propriedade especial, chamada
+                    //'$_delete', com o valor true e, nesse caso, deleta o subdocumento
+                    if(item['$_delete']=== true){
+                        sale.item.id(item._id).deleteOne()
+                    }
+                    else{
+                       //Procura cada propriedade no item de req.body e 
+                      for(let prop in item){
+                        sale.items.id(item._id)[prop] = item[prop]
+                      }
+                    }
+                }
+                // item não existe -> É caso de inserção
+                else {
+                    sale.items.push(item) // Cria um novo item
+                }
+            }
+
+            // Indica que o item foi modificado e deve ser regravado
+            sale.markModified('items')
+
+
+        }
+
+        //Verifica as demais propriedades do pai (sale) por alterações
+        for(let prop in req.body){
+            if(prop !== 'items'){ // Items ja foi processado acima
+                sale[prop] = req.body[prop]
+                sale.markModified(prop)
+            } 
+        }
+
+        const result = await sale.save()
         
         if(result) {
             //Encontrou e atualizou => HTTP 204: No content
